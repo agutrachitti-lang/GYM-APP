080aac2d9b2461677c8582f1d5069e09f5f09dd9
agutrachitti-lang <agutrachitti@gmail.com>
2026-06-16 15:12:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+# Cambiamos la dirección de donde busca la función
+from database.vencimientos import controlar_vencimientos_automaticos
+
+controlar_vencimientos_automaticos()
+
+st.title("Sistema Administrativo")
+
+import streamlit as st
 from database.conexion import controlar_vencimientos_automaticos
 
 # Corremos el control automático apenas arranca el sistema
