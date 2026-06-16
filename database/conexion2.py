f73182ceefa8bc192f99de1ba9c3aa01e77f7ccb
agutrachitti-lang <agutrachitti@gmail.com>
2026-06-16 14:54:33 -0300

    Delete database directory

--- database/conexion2.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-def get_connection():
-    # Esto crea el archivo 'gym.db' automáticamente si no existe
-    conn = sqlite3.connect('gym.db', check_same_thread=False)
-    return conn
