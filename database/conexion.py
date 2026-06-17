1b4dbc0729ffb174e61a77466a1f1c1d67ded36a
agutrachitti-lang <agutrachitti@gmail.com>
2026-06-17 16:09:36 -0300

    Update conexion.py

--- database/conexion.py
+++ database/conexion.py
@@ -2,19 +2,12 @@ import streamlit as st
 import libsql_experimental as sqlite3
 
 def get_connection():
-    try:
-        # Intentamos obtener los secretos
-        db_url = st.secrets["TURSO_DATABASE_URL"]
-        auth_token = st.secrets["TURSO_AUTH_TOKEN"]
-        
-        conn = sqlite3.connect(database=db_url, auth_token=auth_token)
-        conn.check_same_thread = False
-        
-        # Probamos una consulta simple para ver si la tabla existe
-        conn.execute("SELECT 1 FROM Socios LIMIT 1")
-        return conn
-        
-    except Exception as e:
-        # Si falla, mostramos el error real en pantalla
-        st.error(f"¡Error de conexión! Revisa los Secrets en Streamlit Cloud. Detalle: {e}")
-        st.stop()
+    # URL y Token que me pasaste
+    db_url = "libsql://gymdb-agutrachitti-lang.aws-us-east-1.turso.io"
+    auth_token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODE3MTkyNDEsImlkIjoiMDE5ZWQ2YmMtNzAwMS03MzEzLTllOGYtNzcyMjE2NTU3ZmU0IiwicmlkIjoiMTVlNjFkYzUtMGQzYS00ODc1LTg3OWQtMTA0OTMyOTU0ZGZkIn0.Q-6OhbsrqfuHz4ROTrpNVZXNTfCKQM1Jl9FykIsVSVsb2LVyoTY23n6zsoA95GhSS0HAEPkeMZJAdfcMg3deDw"
+    
+    # Abrimos la conexión directamente con esos datos
+    conn = sqlite3.connect(database=db_url, auth_token=auth_token)
+    conn.check_same_thread = False
+    
+    return conn
